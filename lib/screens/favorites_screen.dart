1b0435d55a7b5113fd7d0977ecb9f09f64e2bb96
bragisivarss <sigursteinnbragi@gmail.com>
2023-12-09 21:07:32 +0000

    favorites screen þarf að laga

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -12,19 +14,59 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-      appBar: AppBar(
-        elevation: 5,
-        shadowColor: const Color.fromARGB(116, 255, 13, 13),
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-        title: Text(
-          'Your Favorites',
-          style: TextStyle(
-              color: Theme.of(context).colorScheme.onSecondaryContainer),
-        ),
-        centerTitle: true,
-      ),
+    final db = FirebaseFirestore.instance;
+    final user = FirebaseAuth.instance.currentUser!;
+
+    Future<List<Map<String, dynamic>>> getFavorites() async {
+      CollectionReference favoritesCollection =
+          db.collection('users').doc(user.uid).collection('favorites');
+
+      QuerySnapshot querySnapshot = await favoritesCollection.get();
+
+      return querySnapshot.docs
+          .map((DocumentSnapshot doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+    }
+
+    return FutureBuilder(
+      future: getFavorites(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Text('No favorites found.');
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data![index]['title']),
+                subtitle: Text(snapshot.data![index]['about']),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
+//     }); Scaffold(
+//       backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+//       appBar: AppBar(
+//         elevation: 5,
+//         shadowColor: const Color.fromARGB(116, 255, 13, 13),
+//         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+//         title: Text(
+//           'Your Favorites',
+//           style: TextStyle(
+//               color: Theme.of(context).colorScheme.onSecondaryContainer),
+//         ),
+//         centerTitle: true,
+//       ),
+//     );
+//   }
+// }
