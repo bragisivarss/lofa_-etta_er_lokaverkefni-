c17955fcdb44c8548c77e6947ee122bef18f16fe
bragisivarss <sigursteinnbragi@gmail.com>
2023-12-10 22:04:13 +0000

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:dundaser/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -13,9 +12,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    const ProviderScope(
-      child: App(),
-    ),
+    const App(),
   );
 }
 
